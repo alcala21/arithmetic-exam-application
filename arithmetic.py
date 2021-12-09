6d980ba62e59c9e5cf478f6f5e3d982db51e800e
Carlos Alcala <alcala21@gmail.com>
2021-12-09 16:56:31 -0600

    Stage 3/4. Arithmetic Exam Application.

--- arithmetic.py
+++ arithmetic.py
@@ -1,12 +1,41 @@
-# write your code here
 import random
 
-[a, b] = random.choices(range(2, 10), k=2)
-op = random.choice(['+', '-', '*'])
-outs = {True: "Right!", False: "Wrong!"}
 
-exp = f"{a} {op} {b}"
-print(exp)
-ans = input()
-print(outs[eval(exp) == eval(ans)])
+class AEA:
 
+    def __init__(self):
+        self.total = 5
+        self.count = 0
+        self.right = 0
+        self.nums = list(range(2, 10))
+        self.ops = ['+', '-', '*']
+        self.exp = ""
+        self.val = 0
+
+    def play(self):
+        while self.count < self.total:
+            self.get_input()
+            self.eval_answer()
+            self.count += 1
+        print(f"Your mark is {self.right}/{self.total}.")
+
+    def get_input(self):
+        [a, b] = random.choices(self.nums, k=2)
+        self.exp = f"{a} {random.choice(self.ops)} {b}"
+        print(self.exp)
+        while True:
+            try:
+                self.val = int(input())
+                break
+            except:
+                print("Incorrect format.")
+
+    def eval_answer(self):
+        if eval(self.exp) == self.val:
+            print("Right!")
+            self.right += 1
+        else:
+            print("Wrong!")
+
+
+AEA().play()
